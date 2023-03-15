5b5bee7da985d3281dd4dae9f78f3cf745e237d5
EvaNahir <121136571+EvaNahir@users.noreply.github.com>
2023-03-14 21:17:16 -0300

    Update Comentador.java
    Actualizado.

--- Comentador.java
+++ Comentador.java
@@ -1,9 +1,9 @@
-/*
+ License Headers in Project Properties/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package comentador.java;
+package comentador;
 import java.util.Scanner;
 /**
  *
@@ -14,18 +14,38 @@ public class Comentador {
     /**
      * @param args the command line arguments
      */
+    Scanner opcion = new Scanner(System.in);
+    
     public static void main(String[] args) {
         // TODO code application logic here
-        Scanner sc= new Scanner(System.in);
-        String Nombre = sc.nextLine();
         System.out.println("Elija una opcion: ");
-        String [] calificacion = {"Me gusta", "No me gusta"} ;
-        System.out.println(calificacion[0]);
-        String Reseña = sc.nextLine();
-        System.out.println(Reseña);
+        System.out.println("A) Me gusta");
+        System.out.println("B) No me gusta");
+        System.out.println("S) Salir");
+        Scanner opcion = new Scanner(System.in);
+        Scanner resena = new Scanner(System.in);
+        String resultado=opcion.nextLine();
         
+        switch (resultado) {
+            case "A":
+                System.out.println("Me gusta");
+                System.out.println("Puede dejar una breve reseña para conocer su opinion. Le pedimos que la misma sea expresada en forma respetable: ");
+                System.out.println(resena.nextLine());
+                break;
+                
+            case "B":
+                System.out.println("No me gusta");
+                System.out.println("Puede dejar una breve reseña para conocer su opinion. Le pedimos que la misma sea expresada en forma respetable: ");
+                System.out.println(resena.nextLine());
+                break;
+                
+            case "S":
+                break;
+                
+            default :
+                System.out.println("Opcion invalida");
+            }    
         }
-        
-    }
     
+    }    
 
